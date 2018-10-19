d0fbbef822c4cbc4368b76ac3e65694949737f08
Alberto Olmo Hernandez (Student) <aolmoher@en4166276l.fulton.ad.asu.edu>
2018-10-19 10:34:33 -0700

    PDDL to propositions and propositions to PDDL v1 finished

--- PDDL-propositions.py
+++ PDDL-propositions.py
@@ -1,17 +1,34 @@
 import pddlpy
 
-def get_predicates(action, human_model):
+
+template = """
+(define (domain blocksworld)
+  (:requirements :strips)
+(:predicates (clear ?x)
+             (on-table ?x)
+             (arm-empty)
+             (holding ?x)
+             (on ?x ?y))
+
+%OPERATORS%"""
+
+
+#####################################################################
+# get_predicates(action, human_model)
+# -------------------------------------------------------------------
+#####################################################################
+def get_predicates(action, model_name):
 
     final = []
 
     for eff_prec in ["effect", "precondition"]:
 
         if eff_prec == "effect":
-            operator = "human_model.domain.operators[action.lower()].effect"
+            operator = model_name+".domain.operators[action].effect"
         else:
-            operator = "human_model.domain.operators[action.lower()].precondition"
+            operator = model_name+".domain.operators[action].precondition"
 
-        pos_neg_preds = []
+        pos_neg_precs = []
         for add_del in ["add", "del"]:
             preds = []
             if add_del == "add":
@@ -26,101 +43,80 @@ def get_predicates(action, human_model):
                     aux += "_" + i
 
                 preds.append("action_"+action+"_has_"+add_del+"_"+eff_prec+aux)
-            pos_neg_preds.append(preds)
-        final.append(pos_neg_preds)
+            pos_neg_precs.append(preds)
+        final.append(pos_neg_precs)
 
     return final
 
 
-def pddl_to_propositions():
+#####################################################################
+# pddl_to_propositions()
+# -------------------------------------------------------------------
+#####################################################################
+def pddl_to_propositions(actions, human_model):
     for action in actions:
 
-        [pos_eff, neg_eff], [pos_pred, neg_pred] = get_predicates(action, human_model)
+        [pos_eff, neg_eff], [pos_prec, neg_prec] = get_predicates(action, human_model)
 
-    return [pos_eff, neg_eff], [pos_pred, neg_pred]
+    return [pos_eff, neg_eff], [pos_prec, neg_prec]
 
 
-def propositions_to_pddl(actions, pos_eff, neg_eff, pos_pred, neg_pred):
+#####################################################################
+# def propositions_to_pddl(actions, pos_eff, neg_eff, pos_prec, neg_prec)
+# -------------------------------------------------------------------
+#####################################################################
+def propositions_to_pddl(propositions, parameters):
+    actionList = {}
+    for prop in propositions:
+        aux = prop.split("_")
+        action = aux[1]
+        add_del = aux[3]
+        effect_prec = aux[4]
+        objects = " ".join(aux[5:])
 
-    x = "action_Stack_has_add_effect_clear_?ob"
-
-    actionList = {"pickup":
-                    {
-                        "precondition": [],
-                        "negprecondition": [],
-                        "add-effect": [],
-                        "delete-effect": []
-                    }
-                }
-
-    y = x.split("_")
-    add_del = y[3]
-    effect_prec = y[4]
-    objects = " ".join(y[5:])
-
-    if add_del == "add" and effect_prec == "effect":
-        actionList["pickup"]['add-effect'].append(objects)
-    if add_del == "del" and effect_prec == "effect":
-        actionList["pickup"]['delete-effect'].append(objects)
-    if add_del == "add" and effect_prec == "precondition":
-        actionList["pickup"]['precondition'].append(objects)
-    if add_del == "del" and effect_prec == "precondition":
-        actionList["pickup"]['negprecondition'].append(objects)
-
-    print(actionList)
-
-    exit()
-
-    template = """
-(define (domain blocksworld)
-  (:requirements :strips)
-(:predicates (clear ?x)
-             (on-table ?x)
-             (arm-empty)
-             (holding ?x)
-             (on ?x ?y))
-
-%OPERATORS%
-    """
-
-    for action in actions:
-        actionList[action] = \
-            {
-                "parameters": "TODO",
-                "precondition": pos_pred,
-                "negprecondition": neg_pred,
-                "add-effect": pos_eff,
-                "delete-effect": neg_eff
+        if not actionList.get(action):
+            actionList[action] = {
+                "parameters": " ".join(parameters[action]),
+                "precondition": [],
+                "negprecondition": [],
+                "add-effect": [],
+                "delete-effect": []
             }
 
-        actionList = {"pickup":
-                        {
-                            "parameters": "?ob",
-                            "precondition": ["clear ?ob", "on-table ?ob", "arm-empty"],
-                            "negprecondition": [],
-                            "add-effect": ["holding ?ob"],
-                            "delete-effect": ["clear ?ob", "on-table ?ob", "arm-empty"]
-                        }
-                  }
+        if add_del == "add" and effect_prec == "precondition":
+            actionList[action]['precondition'].append(objects)
+        if add_del == "del" and effect_prec == "precondition":
+            actionList[action]['negprecondition'].append(objects)
+        if add_del == "add" and effect_prec == "effect":
+            actionList[action]['add-effect'].append(objects)
+        if add_del == "del" and effect_prec == "effect":
+            actionList[action]['delete-effect'].append(objects)
 
-    actionString = '\n'.join(['(:action {}\n  :parameters ({})\n  :precondition (and {})\n  :effect (and {}))\n' \
+    actionString = '\n'.join(['(:action {}\n  :parameters ({})\n  :precondition (and {})\n  :effect (and {}))\n'
                              .format(key, actionList[key]['parameters'],
                                      ''.join(['({}) '.format(p) for p in actionList[key]['precondition']]) + '' +
-                                     ''.join(['(not ({})) '.format(p) for p in actionList[key]['negprecondition']]), \
+                                     ''.join(['(not ({})) '.format(p) for p in actionList[key]['negprecondition']]),
                                      '{}{}'.format(
-                                         ''.join(['({}) '.format(p) for p in actionList[key]['add-effect']]), \
+                                         ''.join(['({}) '.format(p) for p in actionList[key]['add-effect']]),
                                          ''.join(
                                              ['(not ({})) '.format(p) for p in actionList[key]['delete-effect']]))) for
                               key in actionList.keys()])
-
 
     print(template.replace('%OPERATORS%',actionString))
 
 
 if __name__ == '__main__':
     human_model = pddlpy.DomainProblem("blocks-domain.pddl", "prob2.pddl")
-    actions = ["Pickup", "Putdown", "Stack", "Unstack"]
+    actions = ["pickup", "putdown", "stack", "unstack"]
+    model_name = "human_model"
 
-    [pos_eff, neg_eff], [pos_pred, neg_pred] = pddl_to_propositions()
-    propositions_to_pddl(actions, pos_eff, neg_eff, pos_pred, neg_pred)
+    parameters = {}
+    for action in actions:
+        parameters[action] = list(human_model.domain.operators[action].variable_list.keys())
 
+    test_propositions = ["action_stack_has_add_effect_clear_?ob",
+         "action_stack_has_del_effect_clear_?ob",
+         "action_pickup_has_add_effect_test_?ob"]
+
+    [pos_eff, neg_eff], [pos_prec, neg_prec] = pddl_to_propositions(actions, model_name)
+    propositions_to_pddl(test_propositions, parameters)
